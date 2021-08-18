119221a33ca733a19bb6cd481eb5911d64910685
Vincent T <vyncode@gmail.com>
2021-08-18 00:01:04 -0400

    need fixes

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/client'
-import { CREATE_USER } from "../utils/mutations";
+
 // import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -13,7 +14,15 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [createUser, { error }] = useMutation(CREATE_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -31,25 +40,20 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await createUser({
-        variables: userFormData,
+      const { data } = await addUser({
+        variables: { ...userFormData },
       });
-
-      if (error) {
-        throw new Error('something went wrong!');
-      }
-      const { token, user } = data.createUser;
-      console.log(user);
-      Auth.login(token);
-      setUserFormData({
-        username: '',
-        email: '',
-        password: '',
-      });
+      console.log(data);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
   };
 
   return (
--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -22,27 +22,26 @@ mutation loginUser($email: String!, $password: String!) {
     }
 }`
 
-export const CREATE_USER = gql`
-mutation createUser($username: String!, $email: String!, $password: String!) {
-    createUser(email: $email, password: $password) {
-        token
-        user
-         {
-             _id
-             username
-             email
-             bookCount
-             savedBooks{
-                bookId
-                authors
-                description
-                title
-                image
-                link
-             }
-         }
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
-}`
+  }
+}`;
 
 export const DELETE_BOOK = gql`
 mutation deleteBook($bookId: String!) {
